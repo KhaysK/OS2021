b4964b480d8c4eeecc66d919dc758f6a179fbd44
Khabib <84040047+KhaysK@users.noreply.github.com>
2021-11-03 03:13:23 +0300

    Week11 solutions

--- Week11/ex1.sh
+++ Week11/ex1.sh
@@ -1,2 +1,10 @@
 rm -f lofs.img
-dd if=/dev/zero of=lofs.img bs=1M count=55
+rm -rf lofsdisk
+
+dd if=/dev/zero of=lofs.img bs=1M count=1000
+mkfs lofs.img
+sudo losetup -f lofs.img
+mkdir lofsdisk
+sudo mount --source lofs.img --target lofsdisk
+
+chmod 777 lofsdisk
